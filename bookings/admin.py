4e1ab39cbab4834653775b3396c1efcef20e012b
KhanRana <rana_khan@outlook.com>
2023-05-24 22:25:29 +0000

    Add search and filter options to the admin page

--- bookings/admin.py
+++ bookings/admin.py
@@ -5,5 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Menu)
 class MenuAdmin(SummernoteModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title','created_on')
     list_filter = ('created_on',)
     summernote_fields = ('content')
