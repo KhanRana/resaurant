a2bba1ea1dd2c29a3465842531ae2ba94ee35bf2
KhanRana <rana_khan@outlook.com>
2023-05-24 17:34:50 +0000

    Include summernote library to style the admin panel

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Menu
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Menu)
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
--- gossip/settings.py
+++ gossip/settings.py
@@ -45,9 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'django_summernote',
     'home',
     'bookings',
-    'cloudinary',
+
+    
 
 ]
 
