1cafcb1967fb68b9f16a77930875c97b8f6b99b8
KhanRana <rana_khan@outlook.com>
2023-05-24 12:16:38 +0000

    Complete the 'Menu' model to add menu items

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
 class Menu(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    price = models.FloatField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(User, related_name='menu_likes', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def num_of_Likes(self):
+        return self.likes.count()
+    
+
