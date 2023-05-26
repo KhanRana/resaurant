86578d37c0e5e52c57f37acd69e85b5fcbe78d67
KhanRana <rana_khan@outlook.com>
2023-05-26 11:42:43 +0000

    Create model and view for user reviews

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Review
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -9,3 +9,13 @@ class MenuAdmin(SummernoteModelAdmin):
     list_display = ('title','created_on')
     list_filter = ('created_on',)
     summernote_fields = ('content')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'email', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields= ('name', 'body', 'email')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
 # Create your views here.
 
@@ -20,3 +22,10 @@ def menu(request):
         'menu': menu_items
     }
     return render(request, 'bookings/menu.html', context)
+
+class ReviewList(generic.ListView):
+    model = Review
+    queryset = Review.objects.filter(approved=1).order_by('-created_on')
+    template_name = 'home.html'
+    paginate_by = 5
+    
