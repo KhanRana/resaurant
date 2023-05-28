d19cbfdd0299e0baa91756f01bea2ff548a5fde5
KhanRana <rana_khan@outlook.com>
2023-05-28 18:11:55 +0000

    Render the booking model to the browser so a user can book a table

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views import generic
-from .models import Review, Menu, Booking
+from .models import Review, Menu
+from .forms import BookTableForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -12,9 +14,20 @@ def menu(request):
     }
     return render(request, 'bookings/menu.html', context=context)
 
+
 @login_required
 def booking(request):
-    return render(request, 'bookings/booking.html')
+    if request.method == 'POST':
+        booking_form = BookTableForm(request.POST)
+        if booking_form.is_valid():
+            booking_form.save()
+            messages.success(request, 'Your table has been booked, We look forward to seeing you!')
+            return redirect('gossip-menu')
+        
+    else:
+        booking_form = BookTableForm()
+    return render(request, 'bookings/booking.html', {'booking_form': booking_form })
+
 
 class ReviewList(generic.ListView):
     model = Review
