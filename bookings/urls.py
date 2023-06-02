ed2cb12d8d9086094880d84ce25f864297573a5b
KhanRana <rana_khan@outlook.com>
2023-06-02 15:16:45 +0000

    Create class-based view for table booking and render it

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import booking as booking_view
-from .views import booking_list
+# from .views import booking as booking_view
+from .views import booking_list, CreateBookingView
 
 urlpatterns = [
-    path('bookings/',booking_view , name='gossip-booking'),
+    # path('bookings/',booking_view , name='gossip-booking'),
     path('user_bookings/',booking_list, name='user-bookings'),
+    path('bookings/new/', CreateBookingView.as_view(), name='create-booking')
 
     ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from django.views import generic
-from django.views.generic import ListView
+from django.views.generic import (ListView, 
+FormView, CreateView)
 from .models import Review, Menu, Table, Booking
 from .forms import BookTableForm
 from django.contrib.auth.decorators import login_required
@@ -16,7 +17,34 @@ def menu(request):
     }
     return render(request, 'bookings/menu.html', context=context)
 
+class CreateBookingView(CreateView):
+    model = Booking
+    fields = ['username', 'table', 'date', 'time']
 
+    def form_valid(self, BookTableForm):
+        data = BookTableForm.cleaned_data
+        table_list = Table.objects.filter(num=data['table'].num)
+        available_tables = []
+        for table in table_list:
+            if check_availability(table, data['date'], data['time']):    
+                 available_tables.append(table)
+            
+        if len(available_tables) > 0:
+            new_table = available_tables[0]
+            new_booking = Booking.objects.create(
+                username=self.request.user,
+                table=new_table,
+                date=data['date'],
+                time=data['time'],)
+            new_booking.save()
+            messages.success(self.request, '''Your table has been booked! 
+                             Please look at our delicious menu and pick your favourites. 
+                             We look forward to seeing you!''')
+            return redirect('gossip-menu')
+        else:
+            messages.error(self.request,'''A booking already exists,
+              please select another table or time''')
+            return redirect('create-booking')
 
 
 class MenuListView(ListView):
@@ -27,34 +55,34 @@ class MenuListView(ListView):
     ordering = ['price']
 
 
-@login_required
-def booking(request):
-    if request.method == 'POST':
-        booking_form = BookTableForm(request.POST)
-        if booking_form.is_valid():
-            data = booking_form.cleaned_data
-            table_list = Table.objects.filter(num=data['table'].num)
-            available_tables = []
-            for table in table_list:
-                if check_availability(table, data['date'], data['time']):    
-                    available_tables.append(table)
+# @login_required
+# def booking(request):
+#     if request.method == 'POST':
+#         booking_form = BookTableForm(request.POST)
+#         if booking_form.is_valid():
+#             data = booking_form.cleaned_data
+#             table_list = Table.objects.filter(num=data['table'].num)
+#             available_tables = []
+#             for table in table_list:
+#                 if check_availability(table, data['date'], data['time']):    
+#                     available_tables.append(table)
             
-            if len(available_tables) > 0:
-                new_table = available_tables[0]
-                new_booking = Booking.objects.create(
-                    username=request.user,
-                    table=new_table,
-                    date=data['date'],
-                    time=data['time'],)
-                new_booking.save()
-                messages.success(request, 'Your table has been booked, We look forward to seeing you!')
-                return redirect('gossip-menu')
-            else:
-                messages.error(request, 'A booking already exists, please select another table or time')
-                return render(request, 'bookings/booking.html', {'booking_form': booking_form })
-    else:
-        booking_form = BookTableForm()
-    return render(request, 'bookings/booking.html', {'booking_form': booking_form })
+#             if len(available_tables) > 0:
+#                 new_table = available_tables[0]
+#                 new_booking = Booking.objects.create(
+#                     username=request.user,
+#                     table=new_table,
+#                     date=data['date'],
+#                     time=data['time'],)
+#                 new_booking.save()
+#                 messages.success(request, 'Your table has been booked, We look forward to seeing you!')
+#                 return redirect('gossip-menu')
+#             else:
+#                 messages.error(request, 'A booking already exists, please select another table or time')
+#                 return render(request, 'bookings/booking.html', {'booking_form': booking_form })
+#     else:
+#         booking_form = BookTableForm()
+#     return render(request, 'bookings/booking.html', {'booking_form': booking_form })
 
 
 @login_required
