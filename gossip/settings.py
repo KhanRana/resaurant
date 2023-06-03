27eb8b35c021d642255984ffa1724214ffa771f0
KhanRana <rana_khan@outlook.com>
2023-06-03 11:13:24 +0000

    Add bootstrap classes to flash messages that would display below the navbar also style user_bookings and booking_form

--- gossip/settings.py
+++ gossip/settings.py
@@ -16,6 +16,7 @@ if os.path.isfile('env.py'):
 import cloudinary_storage
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,3 +162,10 @@ LOGIN_REDIRECT_URL = 'gossip-home'
 LOGIN_URL = 'gossip-login'
 
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
