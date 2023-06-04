cefc60792951b8552c9ae19cf713bed63f77eb76
KhanRana <rana_khan@outlook.com>
2023-06-04 17:50:21 +0000

    Add user profile photo and test it, and also link the cloudinary urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', user_views.register, name='gossip-register'),
     path('profile/', user_views.profile, name='gossip-profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
