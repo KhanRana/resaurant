97f548e527f62c3514976185cc2caa6832f9d885
KhanRana <rana_khan@outlook.com>
2023-06-06 16:20:33 +0000

    Create a date widget for booking_form page for user to select from the calendar

--- bookings/forms.py
+++ bookings/forms.py
@@ -5,3 +5,17 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('name', 'body',)
+
+
+class DateInput(forms.DateInput):
+    """Create date widget for the form field"""
+    input_type = 'date'
+
+class BookingForm(forms.ModelForm):
+    """Booking form to get user input for the booking"""
+    class Meta:
+        model = Booking
+        fields = ['table', 'date', 'time']
+        widgets = {
+            'date': DateInput(),
+        }
