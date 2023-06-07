e490c97034faa510e5221e1c51438f219103f51c
KhanRana <rana_khan@outlook.com>
2023-06-07 11:57:29 +0000

    Add functionality for user to update their profile, including username, email and image

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 from django import forms
 
 class UserRegistrationForm(UserCreationForm):
@@ -8,3 +9,14 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+     class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage as storage 
+
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +13,34 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    
+    def save(self):
+        # Resizing user profile image for the upload
+        super(Profile, self).save()
+        # After save, read the file
+        image_read = storage.open(self.image.name, "r")
+        image = Image.open(image_read)
+        if image.height > 200 or image.width > 200:
+            size = 200, 200
+            
+            # Create a buffer to hold the bytes
+            imageBuffer = BytesIO()
+
+            # Resize  
+            image.thumbnail(size, Image.ANTIALIAS)
+
+            # Save the image as jpeg to the buffer
+            image.save(imageBuffer, image.format)
+
+            # Check whether it is resized
+            image.show()
+
+            # Save the modified image
+            user = Profile.objects.get(pk=self.pk)
+            user.image.save(self.image.name, ContentFile(imageBuffer.getvalue()))
+
+            image_read = storage.open(user.image.name, "r")
+            image = Image.open(image_read)
+
+        image_read.close()
